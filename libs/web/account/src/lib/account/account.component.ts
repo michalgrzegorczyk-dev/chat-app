10804139c4e51adaa43650dea91d15938d2b7f87
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-13 01:07:36 +0200

    init

---
+++ libs/web/account/src/lib/account/account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-account',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.css',
+})
+export class AccountComponent {}

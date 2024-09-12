00b53222cf66ff76694d996225d567816a86c72e
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-13 01:22:41 +0200

    init

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,14 +1,35 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
+// TODO: why tailwind doesn't work by default?
+// https://stackoverflow.com/questions/72360817/nx-workspace-tailwind-is-not-working-in-the-libaries
+// probably __dirname is not working as expected, because i added custom web folder
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), //todo is it useful?
+    join('web/**/!(*.stories|*.spec).{ts,html}'), //todo added this line
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#F3F0FF',
+          100: '#E9E3FF',
+          200: '#9370DB',
+          500: '#7B68EE',
+          600: '#6A5ACD',
+          700: '#6050CB',
+        },
+        danger: {
+          100: '#FFA8A3',
+          200: '#FF7066',
+          300: '#FF5A5F',
+        },
+      },
+    },
   },
   plugins: [],
 };

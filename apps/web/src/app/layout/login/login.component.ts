d5ea267cf453ae77eafd631bba77ecaf30c616fe
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-20 20:11:30 +0200

    fix routing

--- apps/web/src/app/layout/login/login.component.ts
+++ apps/web/src/app/layout/login/login.component.ts
@@ -1,13 +1,12 @@
-import { Component, ChangeDetectionStrategy, OnInit, signal, inject, DestroyRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { NgFor, NgIf, JsonPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AccountWidgetComponent, AccountListComponent } from '@chat-app/feature-account';
 import { User } from '@chat-app/domain';
 import { Router } from '@angular/router';
 import { AuthService } from '@chat-app/util-auth';
-import { first } from 'rxjs';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import { ROUTES } from '../../app.routes';
+import { ROUTES, routing } from '../../app.routes';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
@@ -24,6 +23,6 @@ export class LoginComponent {
 
   async onSelectUser(user: User): Promise<void> {
     this.authService.setUser(user);
-    await this.router.navigate([`/${ROUTES.CHAT}`]);
+    await this.router.navigate([`${routing.chat.url()}`]);
   }
 }

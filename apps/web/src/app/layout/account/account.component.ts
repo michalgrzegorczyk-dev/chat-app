13ecfb6be78943523c9df157ba12ec02f7af86d6
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-22 00:44:24 +0200

    init

--- apps/web/src/app/layout/account/account.component.ts
+++ apps/web/src/app/layout/account/account.component.ts
@@ -1,10 +1,8 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AccountWidgetComponent, AccountListComponent } from '@chat-app/feature-account';
-import { AuthService } from '@chat-app/web/shared/util/auth';
-import { Router, RouterOutlet } from '@angular/router';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'mg-account-layout',
@@ -14,9 +12,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AccountComponent {
-  private readonly authService = inject(AuthService);
-  //todo check leaks with tosignal approach
-  readonly users = toSignal(this.authService.getAllUsers(), { initialValue: [] });
-  private readonly router = inject(Router);
 
+  back() {
+    history.back();
+  }
 }

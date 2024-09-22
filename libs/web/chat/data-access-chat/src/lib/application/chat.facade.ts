a45d4938975295959b8c01653a224714eb733a82
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-23 01:10:01 +0200

    fix

--- libs/web/chat/data-access-chat/src/lib/application/chat.facade.ts
+++ libs/web/chat/data-access-chat/src/lib/application/chat.facade.ts
@@ -1,42 +1,30 @@
-import { Injectable, inject } from '@angular/core';
-import { Message, Conversation, User, ChatStore, MessageSend } from '@chat-app/domain';
-import { DataSyncer } from './data-syncer/data-syncer.service';
+import { ChatStore } from './store/chat.store';
+import { MessageSend } from '../models/message-send.type';
+import { Conversation } from '../models/conversation.type';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatFacade {
-
   private readonly chatStore = inject(ChatStore);
-  private readonly dataSyncer = inject(DataSyncer);
 
-
-  // READ
   readonly messageList = this.chatStore.messageList;
   readonly messageListLoading = this.chatStore.messageListLoading;
   readonly conversationList = this.chatStore.conversationList;
   readonly conversationListLoading = this.chatStore.conversationListLoading;
   readonly selectedConversation = this.chatStore.selectedConversation;
-  readonly selectedConversationLoading = this.chatStore.selectedConversationLoading;
   readonly memberIdMap = this.chatStore.memberIdMap;
 
   sendMessage(messageSend: MessageSend): void {
-    this.chatStore.sendMessage$.next(messageSend)
+    this.chatStore.sendMessage$.next(messageSend);
   }
 
   loadConversationList(): void {
     this.chatStore.loadConversationList$.next();
   }
 
-  setMessageList(messageList: Message[]): void {
-    this.chatStore.setMessageList$.next(messageList);
-  }
-
   selectConversation(conversation: Conversation): void {
     this.chatStore.selectConversation$.next(conversation);
-  }
-
-  setMemberMap(memberMap: Map<string, User>): void {
-    this.chatStore.setMemberIdMap$.next(memberMap);
   }
 }

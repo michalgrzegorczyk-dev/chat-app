de8c730fa23dcd156ae64b42995d16f27e6d7327
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-25 15:21:44 +0200

    fix

--- libs/web/shared/util/environment/src/lib/environment.ts
+++ libs/web/shared/util/environment/src/lib/environment.ts
@@ -3,25 +3,3 @@ export const environment = {
   apiUrl: 'http://localhost:3000',
   websocketUrl: 'http://localhost:3000'
 };
-
-// modul
-// forroot
-// inicjalizacja tokena
-//
-
-// todo maybe
-// export interface EnvironmentConfig {
-//   apiUrl: string;
-//   websocketUrl: string;
-// }
-//
-// export const ENVIRONMENT_CONFIG = new InjectionToken<EnvironmentConfig>('environment.config');
-//
-// if (environment.production) {
-//   enableProdMode();
-// }
-//
-// platformBrowserDynamic([
-//   { provide: ENVIRONMENT_CONFIG, useValue: environment }
-// ]).bootstrapModule(AppModule)
-//   .catch(err => console.error(err));

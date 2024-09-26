46f5f1b62dd05a13372def74ce561662b266ad0b
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-26 21:17:17 +0200

    fix

--- libs/web/chat/data-access-chat/src/lib/application/store/chat.sync.ts
+++ libs/web/chat/data-access-chat/src/lib/application/store/chat.sync.ts
@@ -1,7 +1,12 @@
 import { Injectable, NgZone } from '@angular/core';
 import { MessageSend, ReceivedMessage } from '@chat-app/domain';
-import { Observable, fromEvent, Subject, BehaviorSubject } from 'rxjs';
-import { NotifierService } from '@chat-app/ui-notifier';
+import { fromEvent, Subject, BehaviorSubject } from 'rxjs';
+
+const BROADCAST_CHANNEL_TYPES = {
+  SYNC_REQUEST: 'sync_request',
+  SYNC_DATA: 'sync_data',
+  MESSAGE_SENT: 'message_sent'
+};
 
 @Injectable({ providedIn: 'root' })
 export class ChatSync {
@@ -9,29 +14,23 @@ export class ChatSync {
   private queueSubject = new BehaviorSubject<MessageSend[]>([]);
   queue$ = this.queueSubject.asObservable();
 
-  private readonly notifier: NotifierService;
-  private isOnline$: Observable<Event>;
-  private broadcastChannel: BroadcastChannel;
+  private broadcastChannel = new BroadcastChannel('chat_sync');
   private messageSentSubject = new Subject<ReceivedMessage>();
   messageSent$ = this.messageSentSubject.asObservable();
-  constructor(notifierService: NotifierService, private ngZone: NgZone) {
-    this.notifier = notifierService;
-    this.isOnline$ = fromEvent(window, 'online');
-    this.isOnline$.subscribe(() => {
+
+  constructor(private ngZone: NgZone) {
+    fromEvent(window, 'online').subscribe(() => {
       console.log('Online: Syncing messages');
       this.syncMessages();
     });
 
-    this.broadcastChannel = new BroadcastChannel('chat_sync');
     this.broadcastChannel.onmessage = (event) => {
-      console.log('on message',event.data.type );
-      if (event.data.type === 'sync_request') {
-        console.log('sync');
+      console.log('on message', event.data.type);
+      if (event.data.type === BROADCAST_CHANNEL_TYPES.SYNC_REQUEST) {
         this.broadcastSyncData();
-      } else if (event.data.type === 'sync_data') {
-        console.log(event.data.queue)
+      } else if (event.data.type === BROADCAST_CHANNEL_TYPES.SYNC_DATA) {
         this.receiveSyncData(event.data.queue);
-      } else if (event.data.type === 'message_sent') {
+      } else if (event.data.type === BROADCAST_CHANNEL_TYPES.MESSAGE_SENT) {
         this.handleMessageSent(event.data.message);
       }
     };
@@ -43,28 +42,6 @@ export class ChatSync {
         }
       });
     });
-  }
-
-  private handleMessageSent(message: ReceivedMessage) {
-    this.removeMessage(message);
-    this.messageSentSubject.next(message);
-  }
-
-  private broadcastSyncData() {
-    console.log('lol');
-    this.broadcastChannel.postMessage({
-      type: 'sync_data',
-      queue: this.queueSubject.value
-    });
-  }
-
-  private onPageVisible() {
-    console.log('Page is now visible');
-    this.requestSync();
-  }
-
-  private receiveSyncData(receivedQueue: MessageSend[]) {
-    this.queueSubject.next(receivedQueue);
   }
 
   requestSync() {
@@ -91,6 +68,32 @@ export class ChatSync {
     this.broadcastSyncData();
   }
 
+  sendMessage(message: MessageSend): void {
+    this.sendMessage$.next(message);
+  }
+
+  private handleMessageSent(message: ReceivedMessage) {
+    this.removeMessage(message);
+    this.messageSentSubject.next(message);
+  }
+
+  private broadcastSyncData() {
+    console.log('lol');
+    this.broadcastChannel.postMessage({
+      type: 'sync_data',
+      queue: this.queueSubject.value
+    });
+  }
+
+  private onPageVisible() {
+    console.log('Page is now visible');
+    this.requestSync();
+  }
+
+  private receiveSyncData(receivedQueue: MessageSend[]) {
+    this.queueSubject.next(receivedQueue);
+  }
+
   private syncMessages() {
     const currentQueue = this.queueSubject.value;
     while (currentQueue.length > 0) {
@@ -102,9 +105,5 @@ export class ChatSync {
     }
     this.queueSubject.next(currentQueue);
     this.broadcastSyncData();
-  }
-
-  sendMessage(message: MessageSend): void {
-    this.sendMessage$.next(message);
   }
 }

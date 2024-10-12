dc2d734b3b182f8ae0c2cdf9ab758de58695f7bc
Michał Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-10-12 18:40:28 +0200

    refactor: remove group chats (#13)

--- libs/web/chat/data-access-chat/src/lib/models/conversation.type.ts
+++ libs/web/chat/data-access-chat/src/lib/models/conversation.type.ts
@@ -2,7 +2,6 @@ export interface Conversation {
   conversationId: string;
   avatarUrl: string;
   name: string;
-  chatType: string;
   lastMessageContent: string;
   lastMessageTimestamp: string;
   lastMessageSenderId?: string;

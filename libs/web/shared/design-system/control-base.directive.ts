fde789bbde1fe74ffb9ac83de5872ab774c136d0
Jair292 <jair.pinto1992@gmail.com>
2024-11-01 19:36:40 -0500

    feature: create control base class for app custom controls

---
+++ libs/web/shared/design-system/control-base.directive.ts
@@ -0,0 +1,143 @@
+import { Directive, inject, input, model, OnDestroy, OnInit, output } from "@angular/core";
+import { AbstractControl, ControlContainer, FormArray, FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+
+export const CONTROL_VIEW_PROVIDERS = [
+  {
+    provide: ControlContainer,
+    useFactory: () => inject(ControlContainer, {skipSelf: true}),
+  }
+] as const;
+
+export const CONTROL_COMMON_IMPORTS = [
+  ReactiveFormsModule,
+] as const;
+
+/**
+ * If controlName is provided the control will be added to the parent ControlContainer.
+ * If control is provided the control will be used instead of creating a new one, and will not be injected in ControlContainer.
+ */
+@Directive()
+export abstract class ControlBase<ControlType> implements OnInit, OnDestroy {
+  readonly parentControlContainer = inject(ControlContainer, { skipSelf: true, optional: true });
+
+  /**
+  * @required
+  */
+  readonly id = input.required<string>();
+
+  /**
+  * @optional Define FormControl object.
+  * Can be used inside ControlContainer or directly.
+  */
+  readonly control = model<FormControl<ControlType>>();
+
+  /**
+   * @optional Name for control injection into ControlContainer.
+   * If provided, a new control will be created and injected in closest ControlContainer.
+   * Key collision will ends in control overwrite in ControlContainer (FromGroups).
+  */
+  readonly controlName = input<string>();
+
+  /**
+  * @optional Value for control when providing controlName.
+  * **Required while using controlName**
+  */
+  readonly value = model<ControlType>();
+
+  /**
+   * @readonly Output signal, react to value changes.
+   */
+  readonly valueChange = output<ControlType>();
+
+  ngOnInit(): void {
+    this.validateControlConfig(this.value(), this.controlName(), this.control());
+    this.initControl();
+    this.injectControl();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyControl();
+  }
+
+  changeValue(): void {
+    this.valueChange.emit(this.control()!.value || this.value()!);
+  }
+
+  protected validateControlConfig(value?: ControlType, controlName?: string, control?: AbstractControl): void {
+    const messagePrefix = `Control Component;`;
+    const hasControl = !!control;
+    const hasValue = (value !== null && value !== undefined);
+    const hasControlName = !!controlName;
+
+    if (!hasControlName && !hasControl) {
+      throw new Error(`${messagePrefix} Missing properties to build AbstractControl. Provide "control" or "controlName".`);
+    }
+    if (hasControl && (hasControlName || hasValue)) {
+      throw new Error(`${messagePrefix} "control" can not be used within with "value" or "controlName".`);
+    }
+    if (hasControlName && !hasValue) {
+      throw new Error(`${messagePrefix} Property "value" is required to create instance of Control when using "controlName".`);
+    }
+  }
+
+  /**
+   * @protected
+   * Creates control and initializes it
+   */
+  protected initControl(): void {
+    if (!this.control()) {
+      this.control.set(new FormControl<ControlType>(this.value()!, { nonNullable: true }));
+    }
+  }
+
+  /**
+   * @private
+   * @returns Object with properties for control injection.
+   */
+  private getValidatedControlConfig() {
+    const control = this.control();
+    const controlName = this.controlName();
+    const parentControl = this.parentControlContainer?.control;
+
+    if (!control || !controlName || !parentControl) {
+      return undefined;
+    }
+
+    return { control, controlName, parentControl };
+}
+
+  /**
+   * @protected
+   * Injects control into ControlContainer
+   */
+  protected injectControl(): void {
+    const validatedConfig = this.getValidatedControlConfig();
+    if (!validatedConfig) return;
+
+    const { control, controlName, parentControl } = validatedConfig;
+
+    if (parentControl instanceof FormGroup) {
+      parentControl.addControl(controlName, control);
+    } else if (parentControl instanceof FormArray) {
+      parentControl.push(control);
+    }
+  }
+
+  /**
+   * @protected
+   * Remove control from ControlContainer
+   */
+  protected destroyControl() {
+    const validatedConfig = this.getValidatedControlConfig();
+    if (!validatedConfig) return;
+
+    const { control, controlName, parentControl } = validatedConfig;
+
+    if (parentControl instanceof FormGroup) {
+      parentControl.removeControl(controlName);
+    } else if (parentControl instanceof FormArray) {
+      const index = parentControl.controls.indexOf(control);
+      parentControl.removeAt(index);
+    }
+  }
+}
